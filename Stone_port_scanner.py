1d2767fd873b9398e0377d10b6c3fde26ba8ccfc
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2019-11-26 20:03:40 +0300

    1.7

--- Stone_port_scanner.py
+++ Stone_port_scanner.py
@@ -3,14 +3,13 @@
 #  [ Stolar Studio ]
 #
 
-ver = "0.1.6"
+ver = "0.1.7"
 
-import threading 
+import threading
 import socket
 import os
 import sys
 import configparser
-#import argparse
 
 if not os.path.exists("settings.txt"):
     config = configparser.ConfigParser()
@@ -22,7 +21,6 @@ if not os.path.exists("settings.txt"):
     config.set("Settings", "thread", "true")
     config.add_section("Visual")
     config.set("Visual", "print-logo", "true")
-    #config.set("Visual", "print-ports", "false")
     config.set("Visual", "progress-bar", "true")
     with open("settings.txt", "w") as config_file:
         config.write(config_file)
@@ -35,7 +33,6 @@ try:
     ports_file_name = config.get("Settings", "ports-file-name")
     thread = config.get("Settings", "thread")
     print_logo = config.get("Visual", "print-logo")
-    #print_ports = config.get("Visual", "print-ports")
     progress_bar = config.get("Visual", "progress-bar")
 except:
     print("ERROR READ SETTINGS FILE")
@@ -47,16 +44,15 @@ ports_file_bool = True if ports_file == 'true' or ports_file == 'True' else Fals
 thread_bool = True if thread == 'true' or thread == 'True' else False
 
 print_logo_bool = True if print_logo == 'true' or print_logo == 'True' else False
-#print_ports_bool = True if print_ports == 'true' or print_ports == 'True' else False
 progress_bar_bool = True if progress_bar == 'true' or progress_bar == 'True' else False
 
 if print_logo_bool:
     print("""
-   _____ _                     _____           _      _____                                 
-  / ____| |                   |  __ \         | |    / ____|                                
- | (___ | |_ ___  _ __   ___  | |__) |__  _ __| |_  | (___   ___ __ _ _ __  _ __   ___ _ __ 
+   _____ _                     _____           _      _____
+  / ____| |                   |  __ \         | |    / ____|
+ | (___ | |_ ___  _ __   ___  | |__) |__  _ __| |_  | (___   ___ __ _ _ __  _ __   ___ _ __
   \___ \| __/ _ \| '_ \ / _ \ |  ___/ _ \| '__| __|  \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|
-  ____) | || (_) | | | |  __/ | |  | (_) | |  | |_   ____) | (_| (_| | | | | | | |  __/ |   
+  ____) | || (_) | | | |  __/ | |  | (_) | |  | |_   ____) | (_| (_| | | | | | | |  __/ |
  |_____/ \__\___/|_| |_|\___| |_|   \___/|_|   \__| |_____/ \___\__,_|_| |_|_| |_|\___|_|    """)
 else:
     print("Stone Port Scanner")
@@ -69,11 +65,11 @@ if ports_file_bool and os.path.exists(ports_file_name):
         ports.append(f.read().splitlines())
     print("PORTS : " + ports_file_name + "[ "+str(len(ports[0]))+" ELEMENT ]")
 else:
-    ports = [[21, 22, 23, 25, 38, 43, 80, 999, 109, 110, 115, 118, 119, 143,  
+    ports = [[21, 22, 23, 25, 38, 43, 80, 999, 109, 110, 115, 118, 119, 143,
     194, 220, 443, 540, 585, 591, 1112, 1433, 1443, 3128, 3197,
     3306, 4000, 4333, 5100, 5432, 6669, 8000, 8080, 9014, 9200]]
     print("PORTS : DEFAULT")
-    
+
 if hosts_file_bool and os.path.exists(hosts_file_name):
     hosts = []
     with open(hosts_file_name) as f:
@@ -82,62 +78,58 @@ if hosts_file_bool and os.path.exists(hosts_file_name):
     print(' | '.join(hosts[0]))
 else:
     target = input('Enter host : ')
-    
-print('-' * 35)
 
+print('-' * 35)
 open_ports = []
 
-
-def portscan(port, arr = False):  
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-    s.settimeout(0.5)  
+def portscan(port, arr = False):
+    s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s_tcp.settimeout(0.5); s_udp.settimeout(0.5)
     try:
-        connection = s.connect((target, port))
+        connection_tcp = s_tcp.connect((target, port))
         if not arr == True:
-            print(str(target) + " : " + str(port))
+            print("TCP " + str(target) + " : " + str(port))
         else:
-            open_ports.append(str(target) + " : " + str(port))
+            open_ports.append("TCP " + str(target) + " : " + str(port))
         connection.close()
     except:
         pass
-    
-    
+    try:
+        connection_udp = s_udp.connect((target, port))
+        if not arr == True:
+            print("UDP " + str(target) + " : " + str(port))
+        else:
+            open_ports.append("UDP " + str(target) + " : " + str(port))
+        connection.close()
+    except:
+        pass
+
 def scan_host(target):
     print(target + " START SCANNING")
     for element in ports[0]:
         portscan(int(element))
     print(target + " STOP SCANNING")
-    
+
 def scan_host_multi(target):
     print(target + " START SCANNING")
-    
     if progress_bar_bool:
         pb_i = -2
-        pb_max = len(ports[0]) 
-    
+        pb_max = len(ports[0])
     for element in ports[0]:
-        
-        t = threading.Thread(target=portscan, kwargs={'port': int(element), 'arr': True}) 
+        t = threading.Thread(target=portscan, kwargs={'port': int(element), 'arr': True})
         t.start()
-
         if progress_bar_bool:
             pb_i += 1
-        
             sys.stdout.write('\r')
             part = float(pb_i)/(pb_max - 2)
             symbols_num = int(30 * part)
             sys.stdout.write("SCANNING [%-30s] %3.2f%%" % ('='*symbols_num, part*100))
             sys.stdout.flush()
-
-            #pb_i += 1
     if progress_bar_bool:
         sys.stdout.write("\n")
     while True:
-        
         if threading.active_count() < 3:
-            
-            #sys.stdout.write("\n")
-            
             if len(open_ports) > 0:
                 print('\n'.join(open_ports))
             else:
